3dba1dc2e8a406e35fe569fdbe99d2167ee6e52e
Lordkingsmen <116689235+Lordkingsmen@users.noreply.github.com>
2023-05-08 01:09:06 -0700

    Sprite+UI Changes
    Added Sprites for Slider & Slider Knob, and obtained free assets from the unity store for the text.

---
+++ Personal Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    private bool paused;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
