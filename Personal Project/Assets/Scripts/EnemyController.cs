b2a955bef933aeefcc9d2bda703d8669b79143a6
Lordkingsmen <116689235+Lordkingsmen@users.noreply.github.com>
2023-05-24 15:10:00 -0700

    moving enemy
    moves now

--- Personal Project/Assets/Scripts/EnemyController.cs
+++ Personal Project/Assets/Scripts/EnemyController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private Vector3 mover = new Vector3(0, 0, 0.5f);
+    private int iterationCount = 3;
+    private float moveTime;
     void Start()
     {
-        
+        if (tag == "BasicEnemy")
+        {
+            moveTime = 0.5f;
+        }
     }
 
     void Update()
@@ -17,38 +22,50 @@ public class EnemyController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Spawner")
+        if (other.tag == "Spawner" || other.tag == "Straight")
         {
-            Straight();
-        }
-        else if (other.tag == "Straight")
-        {
-            Straight();
+            StartCoroutine("Straight");
         }
         else if (other.tag == "Right Turn")
         {
-            Straight();
+            StartCoroutine("RightTurn");
         }
         else if (other.tag == "Left Turn")
         {
-            Straight();
+            StartCoroutine("Straight");
         }
         else if (other.tag == "T Merge")
         {
-            Straight();
+            StartCoroutine("Straight");
         }
         else if (other.tag == "T Center")
         {
-            Straight();
+            StartCoroutine("Straight");
         }
         else if (other.tag == "X Path")
         {
-            Straight();
+            StartCoroutine("Straight");
         }
     }
 
-    private void Straight()
+    IEnumerator Straight()
     { 
-        transform.Translate(mover);
+        for(int i = 0; i < iterationCount; i++)
+        {
+            yield return new WaitForSeconds(moveTime);
+            transform.Translate(mover);
+        }
+    }
+    IEnumerator RightTurn()
+    {
+        for (int i = 0; i < iterationCount; i++)
+        {
+            if (i == 0)
+            {
+                transform.Rotate(0.0f, -90.0f, 0.0f);
+            }
+            yield return new WaitForSeconds(moveTime);
+            transform.Translate(mover);
+        }
     }
 }
